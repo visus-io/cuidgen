7b4b893e2d6410e49592662d5fad3195e51441b7
Alan Brault <alan.brault@visus.io>
2023-01-09 14:58:21 -0500

    task: initial implementation
    Signed-off-by: Alan Brault <alan.brault@visus.io>

---
+++ src/cuidgen/Program.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable XAELIB0001
+
+using McMaster.Extensions.CommandLineUtils;
+using Xaevik.Cuid;
+using Xaevik.Cuid.Generator;
+using Xaevik.Cuid.Generator.Resources;
+
+CommandLineApplication app = new();
+
+CommandOption<short> generation = app
+	.Option<short>("-g <generation>", Resources.HelpOpt_G_Descr, CommandOptionType.SingleOrNoValue)
+	.Accepts(a => a.Values("1", "2"));
+
+CommandOption<int> length = app.Option<int>("-l <length>", Resources.HelpOpt_L_Descr, CommandOptionType.SingleOrNoValue)
+	.Accepts(a => a.Range(4, 32));
+
+generation.DefaultValue = 2;
+length.DefaultValue = 24;
+
+app.HelpOption();
+app.VersionOption("-v", VersionHelper.GetVersion(true), VersionHelper.GetVersion(false));
+
+app.OnExecute(() =>
+{
+	switch ( generation.ParsedValue )
+	{
+		case 1:
+			Console.WriteLine(Cuid.NewCuid());
+			break;
+		case 2:
+			Console.WriteLine(new Cuid2(length.ParsedValue));
+			break;
+	}
+});
+
+return app.Execute(args);
---
+++ src/cuidgen/VersionHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Xaevik.Cuid.Generator;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[ExcludeFromCodeCoverage]
+internal static class VersionHelper
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string GetVersion(bool simple)
+	{
+		Version? version = Assembly.GetExecutingAssembly().GetName().Version;
+		if ( version is null )
+		{
+			return string.Empty;
+		}
+
+		return simple ? $"{version.Major}.{version.Minor}.{version.Build}" : version.ToString();
+	}
+}
